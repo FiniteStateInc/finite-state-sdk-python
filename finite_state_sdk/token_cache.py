7121ec14d6de8e96d5db1caa2aa7993ecd73317a
Nicholas Vidovich <nicholas@finitestate.io>
2023-09-21 19:25:50 -0400

    Added get_findings, get_products, get_product_asset_versions, get_software_components, search_sbom, and TokenCache

---
+++ finite_state_sdk/token_cache.py
@@ -0,0 +1,66 @@
+import finite_state_sdk
+import os
+import time
+
+
+"""
+A class for caching Finite State API tokens so that a new token is not required for every run of the script
+Use this in place of finite_state_sdk.get_auth_token
+Example Usage
+---
+token_cache = TokenCache(ORGANIZATION_CONTEXT)
+token = token_cache.get_token(CLIENT_ID, CLIENT_SECRET)
+"""
+
+
+class TokenCache():
+    def __init__(self, organization_context):
+        self.token = None
+        self.organization_context = organization_context
+        self.token_file = f'.tokencache/{self.organization_context}/token.txt'
+
+        # create the token cache directory if it doesn't exist
+        if not os.path.exists('.tokencache'):
+            os.mkdir('.tokencache')
+
+        # create the client directory if it doesn't exist
+        if not os.path.exists(f'.tokencache/{self.organization_context}'):
+            os.mkdir(f'.tokencache/{self.organization_context}')
+
+    def _get_token_from_api(self, client_id, client_secret):
+        # get a new token
+        self.token = finite_state_sdk.get_auth_token(client_id, client_secret)
+
+        # write it to disk
+        with open(self.token_file, 'w') as f:
+            f.write(self.token)
+
+    def get_token(self, client_id, client_secret):
+        # try to read from disk
+        if self.token is None:
+            if os.path.exists(self.token_file):
+                # check how old the file is, if it is more than 24 hours old, delete it
+                if os.path.getmtime(self.token_file) < time.time() - 24 * 60 * 60:
+                    print("Token is more than 24 hours old, deleting it...")
+                    self.invalidate_token()
+
+                    self._get_token_from_api(client_id, client_secret)
+                    return self.token
+                else:
+                    print("Getting saved token from disk...")
+                    with open(self.token_file, 'r') as f:
+                        self.token = f.read()
+
+                    return self.token
+            else:
+                self._get_token_from_api(client_id, client_secret)
+                return self.token
+
+        else:
+            return self.token
+
+    def invalidate_token(self):
+        self.token = None
+        if os.path.exists(self.token_file):
+            os.remove(self.token_file)
+            self.token = None
